c280006695cb0b149e8b1ed9bee84da4a4a7b1c8
lange <lange@synyx.de>
2022-01-23 11:28:55 +0100

    76. JUnit Parameterized Test - Method Provider

--- cl_readme.md
+++ cl_readme.md
@@ -286,3 +286,23 @@ void myRepeatedTestWithDi(TestInfo testInfo, RepitionInfo repititionInfo) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 ```
+
+# 76. JUnit Parameterized Test - Method Provider
+
+```groovy
+
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} [{index}] - {arguments}")
+    @MethodSource("getArgs")
+    void fromMehodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 5, 1),
+                Arguments.of("OH", 2, 8),
+                Arguments.of("MI", 3, 5));
+    }
+
+```
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -3,10 +3,9 @@ package guru.springframework.sfgpetclinic.model;
 import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -78,5 +77,20 @@ class PersonTest implements ModelTests {
     @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
     void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} [{index}] - {arguments}")
+    @MethodSource("getArgs")
+    void fromMehodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 5, 1),
+                Arguments.of("OH", 2, 8),
+                Arguments.of("MI", 3, 5));
     }
 }
